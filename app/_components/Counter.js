eb1d72642989008f9ed0de3aea359519d40af83c
Abdelrahman Khalifa <87104141+AbdoAhmed-Khalifa@users.noreply.github.com>
2024-06-11 13:43:55 +0300

    working on wild oasis website

---
+++ app/_components/Counter.js
@@ -0,0 +1,12 @@
+'use client';
+import { useState } from 'react';
+function Counter({ user }) {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount(c => c + 1)}>{count}</button>
+    </div>
+  );
+}
+
+export default Counter;
