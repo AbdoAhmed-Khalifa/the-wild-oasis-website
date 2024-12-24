7c87c29b3e5a34fa911230dd71d30bed4188009b
Abdelrahamn <abdelrahmanahmedkhalifa99@gmail.com>
2024-12-24 15:02:02 +0200

    upate respnsive

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -3,10 +3,18 @@ import Navigation from '@/app/_components/Navigation';
 
 function Header() {
   return (
-    <div className="drawer z-50">
+    <div className="drawer z-50 ">
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         <div className="navbar w-full ">
+          <div className="mx-2 flex-1 px-2">
+            <Logo />
+          </div>
+          <div className="hidden flex-none lg:block">
+            <ul className="menu menu-horizontal text-xl items-center">
+              <Navigation hide={true} />
+            </ul>
+          </div>
           <div className="flex-none lg:hidden ">
             <label
               htmlFor="my-drawer-3"
@@ -28,23 +36,15 @@ function Header() {
               </svg>
             </label>
           </div>
-          <div className="mx-2 flex-1 px-2">
-            <Logo />
-          </div>
-          <div className="hidden flex-none lg:block">
-            <ul className="menu menu-horizontal text-xl items-center">
-              <Navigation hide={true} />
-            </ul>
-          </div>
         </div>
       </div>
       <div className="drawer-side">
         <label
           htmlFor="my-drawer-3"
-          aria-label="close sidebar"
+          aria-label="close sidebar "
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-base-200 min-h-full w-80 p-4 text-xl space-y-4">
+        <ul className="menu bg-base-200 min-h-full w-72 p-4 text-xl space-y-4">
           <li className="mb-6 border-b-2 border-base-900">
             <Logo />
           </li>
